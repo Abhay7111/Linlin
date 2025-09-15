118666b48650dca08495f7f2c7935192a4e35c76
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2025-09-16 01:06:09 +0530

    app created

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div className='w-full h-screen bg-[#D9D9D9]'>Index</div>
+  )
+}
+
+export default Index
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './Pages/Index.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
