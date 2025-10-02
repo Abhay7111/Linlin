309d83468f33de122fdf880ec47514ad053dc1b8
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2025-10-02 19:36:57 +0530

    footer updated

--- src/Components/Foooter.components.jsx
+++ src/Components/Foooter.components.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Foooter_components() {
   return (
-    <div className='bg-[#d47b8d] w-full min-h-10 flex items-center justify-between px-5'>
-     <h1 className='text-2xl logo font-bold'>Linlin</h1>
+    <div className='bg-transparent w-full min-h-10 flex items-center justify-between px-5'>
+     <a href='/' className='text-2xl logo font-bold'>Linlin</a>
      <div className='flex items-center justify-between gap-4 text-zinc-900'>
       <h1 className='text-sm font-medium'>About</h1>
       <h1 className='text-sm font-medium'>Blog</h1>
-      <h1 className='text-sm font-medium'>Buy</h1>
+      <h1 className='text-sm font-medium'>Grab now</h1>
      </div>
      <div className='flex items-center justify-between gap-4'>
-      <h1 className='text-sm font-medium text-zinc-900'>Grab it now</h1>
+      <h1 className='text-xs font-normal text-zinc-900'>This is showcase project by <a href='https://www.github.com/Abhay7111' className='text-zinc-900'>Abhay7111</a></h1>
      </div>
     </div>
   )
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,11 +4,11 @@ import { NavLink } from 'react-router-dom'
 function Nav() {
   return (
     <div className='w-full h-15 flex items-center justify-between px-3'>
-     <NavLink to='/' className='text-3xl font-bold text-zinc-800 related logo '>Linlin</NavLink>
+     <a href='/' className='text-3xl font-bold text-zinc-800 related logo '>Linlin</a>
      <div className='flex items-center justify-start gap-4'>
           <NavLink to='/about' className='text-sm font-medium hover:text-zinc-800 text-zinc-600 '>About</NavLink>
           <NavLink to='/blog' className='text-sm font-medium hover:text-zinc-800 text-zinc-600 '>Blog</NavLink>
-          <NavLink to='/shop' className='text-sm font-medium hover:text-zinc-800 text-zinc-600 '>Buy</NavLink>
+          <NavLink to='/shop' className='text-sm font-medium hover:text-zinc-800 text-zinc-600 '>Grab now</NavLink>
      </div>
      <div>
           <NavLink to='/login' className='text-xs font-medium px-4 py-1 related-bg rounded-full opacity-80 hover:opacity-100 text-white '>Login</NavLink>
