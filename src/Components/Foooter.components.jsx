153098a6c170fa38465b8753b977880e44938bc5
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2025-10-14 21:40:11 +0530

    Footer updated

--- src/Components/Foooter.components.jsx
+++ src/Components/Foooter.components.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom'
 
 function Foooter_components() {
   return (
-    <div className='bg-transparent w-full min-h-10 flex items-center justify-between px-5'>
-     <a href='/' className='text-2xl logo font-bold'>Linlin</a>
+    <div className='bg-transparent w-full min-h-10 flex md:flex-row flex-col items-center justify-between px-5 gap-3 md:gap-0'>
+     <a href='/' className=' text-5xl md:text-2xl logo font-bold hover:text-[#DB60A9]'>Linlin</a>
      <div className='flex items-center justify-between gap-4 text-zinc-900'>
-      <h1 className='text-sm font-medium'>About</h1>
-      <h1 className='text-sm font-medium'>Blog</h1>
-      <h1 className='text-sm font-medium'>Grab now</h1>
+      <NavLink to={`/`} className=' cursor-pointer text-sm font-medium'>About</NavLink>
+      <NavLink to={`/`} className=' cursor-pointer text-sm font-medium'>Blog</NavLink>
+      <NavLink to={`/`} className=' cursor-pointer text-sm font-medium'>Grab now</NavLink>
      </div>
      <div className='flex items-center justify-between gap-4'>
       <h1 className='text-xs font-normal text-zinc-900'>This is showcase project by <a href='https://www.github.com/Abhay7111' className='text-zinc-900'>Abhay7111</a></h1>
